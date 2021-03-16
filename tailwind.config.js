3d091b52b057570c05acdfc9d516177fe4b08e69
trident-kanda <tridentkandayuki@gmail.com>
2021-03-17 04:31:38 +0900

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  purge: ["./pages/*.{js,ts,jsx,tsx}", "./components/*.{js,ts,jsx,tsx}"],
+  purge: [
+    "./pages/index.tsx",
+    "./components/Container.tsx",
+    "./components/Header.tsx",
+    ,
+    "./components/JobList.tsx",
+    ,
+    "./components/Skill.tsx",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
