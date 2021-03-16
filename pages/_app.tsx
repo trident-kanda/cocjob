2b92f65a01dd5562f31431954a3f0274ff56ab60
trident-kanda <tridentkandayuki@gmail.com>
2021-03-17 04:46:03 +0900

    tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/dist/next-server/lib/router/router";
-import "tailwindcss/tailwind.css";
+// import "tailwindcss/tailwind.css";
+import "../style/global.scss";
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
