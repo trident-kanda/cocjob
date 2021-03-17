98c47f4eef30150ed4912e01581867fb45b1c09e
trident-kanda <tridentkandayuki@gmail.com>
2021-03-18 01:52:48 +0900

    title

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>COCjob</title>
+        <title>CoCJob</title>
       </Head>
       <Header />
       <Container>
